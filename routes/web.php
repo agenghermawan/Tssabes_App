22328f573c86ff028f2bd5c12ecc2fd2a0b30a0e
Hermawan <agenghermawan16@gmail.com>
2022-02-28 10:56:19 +0700

    Tssabes App

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\User\HomeController as UserHomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// User
+Route::get('/', UserHomeController::class);
+
+
+
+// Admin
