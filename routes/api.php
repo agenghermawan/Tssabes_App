54078a0b209d6bd97fa719ce7bb9a07ff6d414f0
Hermawan <agenghermawan16@gmail.com>
2022-03-31 13:46:51 +0700

    push

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('logout-api',[RegisterController::class,'logout']);
 });
 
